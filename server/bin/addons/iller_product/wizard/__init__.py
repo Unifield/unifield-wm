de7843311512567753133b21ef7422622b958639
weyl <>
2010-12-13 10:47:26 +0000

    Fusion de la branche gestion_tarif dans le trunk

--- server/bin/addons/iller_product/wizard/__init__.py
+++ server/bin/addons/iller_product/wizard/__init__.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF8 -*-
 
 import wizard_pricelist_configure_promo
+import wizard_pricelist_configure_tarif_special_client
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
--- server/bin/addons/iller_product/wizard/wizard_pricelist_configure_tarif_special_client.py
+++ server/bin/addons/iller_product/wizard/wizard_pricelist_configure_tarif_special_client.py
@@ -10,21 +10,24 @@ from datetime import timedelta
 
 
 _configure_form = """<?xml version="1.0" encoding="utf-8" ?>
-<form string="Configurer une promo">
+<form string="Configurer un tarif spécial pour un client">
     <separator colspan="4" string="Informations Generales" />
     <field name="title" colspan="4" />
+    <field name="client" required="1" />
+    <newline/>
     <field name="start_date" required="1" />
     <field name="end_date" required="1" />
-    <separator colspan="4" string="Produits" />
-    <field name="product_ids" nolabel="1" colspan="4" width="1000" height="450" />
+    <separator colspan="4" />
+    <field name="products" nolabel="1" colspan="4" width="1000" height="450" />
 </form>"""
 
 
 _configure_fields = {
-        'title': {'type': 'char', 'size': 64, 'string': 'Nom de la promo', 'required': True},
+        'title': {'type': 'char', 'size': 64, 'string': 'Nom du tarif', 'required': True},
+        'client': {'type': 'many2one', 'relation': 'res.partner', 'string': 'Client'},
         'start_date': {'type': 'date', 'required': True, 'string': 'Date de debut'},
         'end_date': {'type': 'date', 'required': True, 'string': 'Date de fin'},
-        'product_ids': {'type': 'many2many', 'relation': 'product.product', 'string': 'Produits'},
+        'products': {'type': 'one2many', 'relation': 'product.tarif.special.client', 'string': 'Produits'},
     }
 
 _error_date_form = """<?xml version="1.0" encoding="utf-8" ?>
@@ -35,31 +38,31 @@ _error_date_form = """<?xml version="1.0" encoding="utf-8" ?>
 
 _error_product_form = """<?xml version="1.0" encoding="utf-8" ?>
     <form string="Erreur sur les produits">
-    <label colspan="4" string="Vous devez au moins introduire un produit dans la promo !" />
+    <label colspan="4" string="Vous devez au moins introduire un produit dans le tarif !" />
     </form>
 """
 
 
-class wizard_configure_promo(wizard.interface):
+class wizard_configure_tarif_special_client(wizard.interface):
 
 
     def _valid_form(self, cr, uid, data, args, context={}):
         '''
             Vérifie que la date de début est inférieur à la date de fin
-            et qu'au moins un produit est fourni pour la promo
+            et qu'au moins un produit est fourni pour le tarif
         '''
         if data['form']['start_date'] > data['form']['end_date']:
             return 'error_date'
-        if len(data['form']['product_ids'][0][2]) < 1:
+        if len(data['form']['products']) == 0:
             return 'error_product'
         return 'create'
 
 
 
-    def _define_promo(self, cr, uid, data, list):
+    def _define_tarif_special_client(self, cr, uid, data, pricelist_id, context):
         '''
-            Créé la nouvelle version de liste de prix correspondant à la promo pour
-            la liste de prix list
+            Créé la nouvelle version de liste de prix correspondant au tarif spécial pour
+            la liste de prix
         '''
         version_obj = pooler.get_pool(cr.dbname).get('product.pricelist.version')
         item_obj = pooler.get_pool(cr.dbname).get('product.pricelist.item')
@@ -68,31 +71,31 @@ class wizard_configure_promo(wizard.interface):
         end_date = data['form']['end_date']
         start_date = data['form']['start_date']
 
-        ## La version précédente s'arrête à j-1 du début de la promo
+        ## La version précédente s'arrête à j-1 du début du tarif spécial
         n_end_date = (datetime.strptime(end_date, '%Y-%m-%d')+timedelta(days=1)).strftime('%Y-%m-%d')
-        ## La version précédente démare à j+1 de la fin de la promo
+        ## La version précédente démarre à j+1 de la fin du tarif spécial
         n_start_date = (datetime.strptime(start_date, '%Y-%m-%d')-timedelta(days=1)).strftime('%Y-%m-%d')
 
 
         ## On cherche la version de base
         base_version = False
-        base_ids = version_obj.search(cr, uid, [('pricelist_id', '=', list.id), ('base_ok', '=', True)])
+        base_ids = version_obj.search(cr, uid, [('pricelist_id', '=', pricelist_id), ('base_ok', '=', True)])
         if not base_ids:
-            base_ids = version_obj.search(cr, uid, [('pricelist_id', '=', list.id)])
+            base_ids = version_obj.search(cr, uid, [('pricelist_id', '=', pricelist_id)])
             if not base_ids:
                 return False
         base_version = base_ids[0]
 
-        # On cherche si la promo englobe une ou plusieurs promos existantes
-        included_ids = version_obj.search(cr, uid, [('pricelist_id', '=', list.id), \
+        # On cherche si le tarif englobe une ou plusieurs promos existantes
+        included_ids = version_obj.search(cr, uid, [('pricelist_id', '=', pricelist_id), \
                                                     ('date_end', '<=', end_date), \
                                                     ('date_start', '>=', start_date)])
         if included_ids:
             for included_id in included_ids:
                 version_obj.unlink(cr, uid, [included_id])
 
-        ## On cherche si la promo se situe à l'intérieur d'une version existante
-        version_ids = version_obj.search(cr, uid, [('pricelist_id', '=', list.id), \
+        ## On cherche si le tarif se situe à l'intérieur d'une version existante
+        version_ids = version_obj.search(cr, uid, [('pricelist_id', '=', pricelist_id), \
                                                    ('date_start', '<=', start_date),\
                                                    ('date_end', '>=', end_date)])
 
@@ -107,13 +110,14 @@ class wizard_configure_promo(wizard.interface):
                                                                'base_ok': False,
                                                                'name': v_data.get('name')})
             version_obj.write(cr, uid, [next_id], {'active': True})
-        ## On cherche si la promo est à cheval sur deux versions existantes
+
+        ## On cherche si le tarif est à cheval sur deux versions existantes
         else:
-            before_ids = version_obj.search(cr, uid, [('pricelist_id', '=', list.id), \
+            before_ids = version_obj.search(cr, uid, [('pricelist_id', '=', pricelist_id), \
                                                       ('date_start', '<', start_date), \
                                                       ('date_end', '>', start_date), \
                                                       ('date_end', '<', end_date)])
-            after_ids = version_obj.search(cr, uid, [('pricelist_id', '=', list.id), \
+            after_ids = version_obj.search(cr, uid, [('pricelist_id', '=', pricelist_id), \
                                                      ('date_end', '>', end_date), \
                                                      ('date_start', '<', end_date), \
                                                      ('date_start', '>', start_date)])
@@ -125,96 +129,70 @@ class wizard_configure_promo(wizard.interface):
         return version_obj.copy(cr, uid, base_version, {'date_start': start_date, 
                                                         'date_end': end_date, 
                                                         'base_ok': False,
-                                                        'name': name})
+                                                        'name': name} )
 
 
-    def _create_item(self, cr, uid, data, version_id, type='blanche'):
+    def _create_item(self, cr, uid, data, version_id):
         '''
-            Créer les différentes lignes de prix en fonction des produits et
-            du type de promo
+            Créer les différentes lignes de prix en fonction des produits
         '''
         pool_obj = pooler.get_pool(cr.dbname)
         item_obj = pool_obj.get('product.pricelist.item')
         prod_obj = pool_obj.get('product.product')
-        product_ids = data['form']['product_ids'][0][2]
+
+        products = data['form']['products']
 
         base = 1
-        bareme = 15
-        coeff = 1.136300
         items = []
-        
-        if type == 'blanche':
-            ## On recherche le type de prix qui correspond au prix
-            ## promo blanche
-            type_ids = pool_obj.get('product.price.type').search(cr, uid, [('name', '=', 'Prix promo blanche')])
-            if type_ids:
-                base = type_ids[0]
+        type_ids = pool_obj.get('product.price.type').search(cr, uid, [('name', '=', 'Prix Spécial')])
+        if type_ids:
+            base = type_ids[0]
 
-            ## Si la promo est de type blanche, on applique
-            ## le prix promo blanche pour chaque produit
-            for product in product_ids:
-                p_data = prod_obj.read(cr, uid, product, ['name'])
-                item_id = item_obj.create(cr, uid, {'sequence': 1,
-                                                    'name': p_data.get('name'), 
-                                                    'product_id': product,
-                                                    'base': base,
-                                                    'price_version_id': version_id})
-                items.append(item_id)
+        for product in products:
+            print "prix special = %s" %product[2].get('prix_special')
+            product_id = product[2].get('product_id')
+            p_data = prod_obj.read(cr, uid, product_id, ['name'])
+# La règle standard est: Prix de vente = Prix de base * (1 + coeff) + surcharge
+# Pour un tarif spécial, le coeff vaut -1 est la surcharge est égale au prix spécial
+# Donc Prix de vente = Prix de base * (1-1) + prix spécial = prix spécial
+# Le prix de base importe peu car il n'intervient pas dans le calcul
+            item_id = item_obj.create(cr, uid, {'sequence': 1,
+                                                'name': p_data.get('name'), 
+                                                'product_id': product_id,
+                                                'base': base,
+                                                'price_discount' :-1.0,
+                                                'price_surcharge': product[2].get('prix_special'),
+                                                'price_version_id': version_id})
+            items.append(item_id)
 
-        elif type == 'jaune':
-            ## On recherche le bareme c15
-            bareme_ids = pool_obj.get('product.pricelist.bareme').search(cr, uid, [('name', '=', 'c13')])
-            if bareme_ids:
-                bareme = bareme_ids[0]
-                coeff = pool_obj.get('product.pricelist.bareme').read(cr, uid, bareme, ['valeur']).get('valeur', 1.136300)
-
-            ## On recherche le type de prix qui correspond au prix de vente classique
-            type_ids = pool_obj.get('product.price.type').search(cr, uid, [('name', '=', 'Public Price')])
-            if type_ids:
-                base = type_ids[0]
-
-            ## Si la promo est de type jaune, on applique
-            ## le barème c15 pour chaque produit
-            for product in product_ids:
-                p_data = prod_obj.read(cr, uid, product, ['name'])
-                item_id = item_obj.create(cr, uid, {'sequence': 1,
-                                                    'name': p_data.get('name'), 
-                                                    'product_id': product,
-                                                    'base': base,
-                                                    'bareme_id': bareme,
-                                                    'price_discount': coeff-1,
-                                                    'price_version_id': version_id})
-                items.append(item_id)
         return items
 
 
-    def _create_promo(self, cr, uid, data, args, context={}):
+    def _create_tarif_special_client(self, cr, uid, data, args, context={}):
         '''
             Créer les différentes versions et lignes de prix
         '''
         pool_obj = pooler.get_pool(cr.dbname)
         pricelist_obj = pool_obj.get('product.pricelist')
         version_obj = pool_obj.get('product.pricelist.version')
+        client_obj = pool_obj.get('res.partner')
 
-        ## On récupère toutes les listes de prix où les promos s'appliquent (promo jaune ou blanche cochée)
-        blanche_ids = pricelist_obj.search(cr, uid, [('promo_blanche', '=', True), ('type', '=', 'sale')])
-        jaune_ids = pricelist_obj.search(cr, uid, [('promo_jaune', '=', True), ('type', '=', 'sale')])
+        ## On récupère la liste de prix associée au client et on la duplique 
+        client = client_obj.browse(cr, uid, data['form']['client']) 
+        pricelist_id = client.property_product_pricelist.id
+        new_pricelist_id = pricelist_obj.copy(cr, uid, pricelist_id, {'name': data['form']['title']})
 
         ## On cherche la version de base pour la liste de prix
         ## On cherche la version en cours pendant la promo
         ## On lui donne une date de fin qui correspond au début-1jour de la promo
-        for list in pricelist_obj.browse(cr, uid, blanche_ids):
-#        for list in pricelist_obj.browse(cr, uid, data['ids']):
-            new_version = self._define_promo(cr, uid, data, list)
-            if new_version:
-                version_obj.write(cr, uid, [new_version], {'active': True, 'name': data['form']['title']})
-                new_items = self._create_item(cr, uid, data, new_version, 'blanche')
 
-        for list in pricelist_obj.browse(cr, uid, jaune_ids):
-            new_version = self._define_promo(cr, uid, data, list)
-            if new_version:
-                version_obj.write(cr, uid, [new_version], {'active': True, 'name': data['form']['title']})
-                new_items = self._create_item(cr, uid, data, new_version, 'jaune')
+        new_version = self._define_tarif_special_client(cr, uid, data, new_pricelist_id,context)
+        if new_version:
+            version_obj.write(cr, uid, [new_version], {'active': True, 'name': data['form']['title']})
+            new_items = self._create_item(cr, uid, data, new_version)
+
+        # On associe la nouvelle liste de prix au client
+        client_obj.write(cr, uid, client.id, {'property_product_pricelist': new_pricelist_id})
 
         return {}
 
@@ -225,7 +203,7 @@ class wizard_configure_promo(wizard.interface):
             'result': {'type': 'form',
                        'arch': _configure_form,
                        'fields': _configure_fields,
-                       'state': [('end', 'Annuler'),('choice', 'Creer les promos')]},
+                       'state': [('end', 'Annuler'),('choice', 'Creer le tarif')]},
         },
         'choice': {
             'actions': [],
@@ -249,12 +227,12 @@ class wizard_configure_promo(wizard.interface):
         'create': {
             'actions': [],
             'result': {'type': 'action',
-                        'action': _create_promo,
+                        'action': _create_tarif_special_client,
                         'state': 'end'},
         }
     }
 
-wizard_configure_promo('pricelist.configure.promo')
+wizard_configure_tarif_special_client('pricelist.configure.tarif.special.client')
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
