265c20dae74d87870116614d5145864c4a588592
weyl <>
2010-12-13 10:49:50 +0000

    Fusion de la branche gestion_tarif dans le trunk

---
+++ server/bin/addons/iller_product/wizard/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: UTF8 -*-
+
+import wizard_pricelist_configure_promo
+import wizard_pricelist_configure_tarif_special_client
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
