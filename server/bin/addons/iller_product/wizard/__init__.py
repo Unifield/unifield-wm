abb6f55d204a90c6a40f5171126f80ef527439cd
qt <>
2011-01-31 18:33:31 +0000

    [IMP] Suppression d'un wizard

--- server/bin/addons/iller_product/wizard/__init__.py
+++ server/bin/addons/iller_product/wizard/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: UTF8 -*-
 
-import wizard_pricelist_configure_promo
 import wizard_pricelist_configure_tarif_special_client
 import wizard_nouveau_prix_achat
 import wizard_export_tarif_hilton
