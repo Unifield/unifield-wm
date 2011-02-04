ecb17c338b2b820ace84de375a4af22f83235bc2
qt <>
2011-02-04 15:54:26 +0000

    [IMP] Gestion des cas restants pour le calcul des commissions :         * Cas des collectivités         * Cas des clients autorisés         * Cas du super utilisateur DISTRI5

--- server/bin/addons/iller_sale/iller_commission.py
+++ server/bin/addons/iller_sale/iller_commission.py
@@ -89,6 +89,8 @@ class iller_commission_line(osv.osv):
         '''
         bareme_obj = self.pool.get('product.pricelist.bareme')
         user_obj = self.pool.get('res.users')
+        role_obj = self.pool.get('res.roles')
+        model_data_obj = self.pool.get('ir.model.data')
         order_obj = self.pool.get('sale.order')
 
         if not lines:
@@ -116,22 +118,37 @@ class iller_commission_line(osv.osv):
 
             ## Si le prix inscrit est compris dans la fourchette des barèmes
             if bareme_below and bareme_above:
-                print 'entre 2 barèmes'
                 taux_comm = (bareme_below.taux_com+bareme_above.taux_com)/2
                 return taux_comm*l.get('unit_price')*l.get('qty')
 
             ## Si le prix est supérieur au prix de vente multiplié par le plus grand coeff., le taux de commission est de 1%
             if not bareme_above:
-                print 'prix inférieur'
                 return l.get('unit_price')*l.get('qty')*0.01
 
             ## Si le prix est inférieur au prix de vente multiplié par le plus petit coeff.
             if not bareme_below:
-                print 'prix supérieur'
-                ## TODO: On vérifie si le client ne fait pas partie de la liste des clients autorisés
-                ## TODO: On vérifie si le client n'est pas une collectivité
+                order = order_obj.browse(cr, uid, l.get('order_id'))
+                ## On vérifie si le client ne fait pas partie de la liste des clients autorisés
+                if order.partner_id.depassement:
+                    return l.get('unit_price')*l.get('qty')*0.01
 
-                ## TODO: On vérifie les droits de l'utilisateur
+                ## On vérifie si le client n'est pas une collectivité
+                model_data_ids = model_data_obj.search(cr, uid, [('name', '=', 'pricelist_tarif_collectivite'), ('module', '=', 'iller_product'), ('model', '=', 'product.pricelist')])
+                model_datas = model_data_obj.read(cr, uid, model_data_ids, ['res_id'])
+                if model_datas and len(model_datas) > 0:
+                    if order.pricelist_id.id == model_datas[0].get('res_id', False):
+                        return l.get('unit_price')*l.get('qty')*0.01
+
+                ## On vérifie les droits de l'utilisateur
+                model_data_ids = model_data_obj.search(cr, uid, [('name', '=', 'res_roles_super_salesman'), ('model', '=', 'res.roles')])
+                model_datas = model_data_obj.read(cr, uid, model_data_ids, ['res_id'])
+                if model_datas and len(model_datas) > 0:
+                    user = user_obj.browse(cr, uid, uid)
+                    ## Si l'utilisateur a le rôle DISTRI5
+                    for role in user.roles_id:
+                        if role.id == model_datas[0].get('res_id', False):
+                            return l.get('unit_price')*l.get('qty')*0.01
+
                 ## Dans tous les autres cas, on retourne une erreur
                 raise osv.except_osv('Erreur', u'Vous ne pouvez pas avoir un prix unitaire inférieur au prix de vente du produit multiplié par le barème c19 -- Produit : %s' %l.get('name'))
                 return False
@@ -164,6 +181,20 @@ class iller_sale_commission(osv.osv):
 
 iller_sale_commission()
 
+
+class iller_partner(osv.osv):
+    _name = 'res.partner'
+    _inherit = 'res.partner'
+
+    _columns = {
+        'depassement': fields.boolean(string='Dépassement autorisé ?', help='Si la case est cochée, n\'importe quel utilisateur pourra vendre un produit à ce partenaire avec un prix inférieur au plus petit barème'),
+    }
+
+    _defaults = {
+        'depassement': lambda *a: False,
+    }
+
+iller_partner()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
