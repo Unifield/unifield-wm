f53b9ac5a5b41aa36df3262a3855af2c8069f1d5
qt <>
2011-02-14 08:57:12 +0000

    [IMP] Ajout du wizard de modification des coefficients de promo blanche avec le wizard de modification des coefficients de départ

--- server/bin/addons/iller_product/wizard/wizard_modif_coeff.py
+++ server/bin/addons/iller_product/wizard/wizard_modif_coeff.py
@@ -10,6 +10,7 @@ class wizard_choose_category(osv.osv_memory):
 
     _columns = {
         'category_id': fields.many2one('product.category', string='Catégorie', required=True),
+        'type': fields.selection([('depart', 'Coeff. départ'), ('blanche', 'Coeff. Promo')], string='Coefficient', required=True),
     }
 
     def _choose(self, cr, uid, ids, context={}):
@@ -23,17 +24,24 @@ class wizard_choose_category(osv.osv_memory):
         coeffs = []
 
         for cat in self.browse(cr, uid, ids):
-            context.update({'category_id': cat.category_id.id, 'wizard': 1})
-
-            product_ids = product_obj.search(cr, uid, [('categ_id', '=', cat.category_id.id), ('coeff_depart', 'not in', coeffs)], offset=0, limit=1)
-            while product_ids:
-                for p in product_obj.browse(cr, uid, product_ids):
-                    coeffs.append(p.coeff_depart)
+            context.update({'category_id': cat.category_id.id, 'wizard': 1, 'type': cat.type})
+            if cat.type and cat.type == 'depart':
                 product_ids = product_obj.search(cr, uid, [('categ_id', '=', cat.category_id.id), ('coeff_depart', 'not in', coeffs)], offset=0, limit=1)
+                while product_ids:
+                    for p in product_obj.browse(cr, uid, product_ids):
+                        coeffs.append(p.coeff_depart)
+                    product_ids = product_obj.search(cr, uid, [('categ_id', '=', cat.category_id.id), ('coeff_depart', 'not in', coeffs)], offset=0, limit=1)
+
+            elif cat.type and cat.type == 'blanche':
+                product_ids = product_obj.search(cr, uid, [('categ_id', '=', cat.category_id.id), ('coeff_blanche', 'not in', coeffs)], offset=0, limit=1)
+                while product_ids:
+                    for p in product_obj.browse(cr, uid, product_ids):
+                        coeffs.append(p.coeff_blanche)
+                    product_ids = product_obj.search(cr, uid, [('categ_id', '=', cat.category_id.id), ('coeff_blanche', 'not in', coeffs)], offset=0, limit=1)
 
             coeffs.sort()
             for c in coeffs:
-                coeff_obj.create(cr, uid, {'name': c, 'coeff_new': c, 'category_id': cat.category_id.id}, context=context)
+                coeff_obj.create(cr, uid, {'name': c, 'coeff_new': c, 'category_id': cat.category_id.id, 'type': cat.type}, context=context)
 
         return {'type': 'ir.actions.act_window',
                 'res_model': 'wizard.modif.coeff',
@@ -54,6 +62,7 @@ class wizard_modif_coeff(osv.osv_memory):
         'name': fields.float(digits=(16,2), string='Coeff. Initial', readonly=True),
         'coeff_new': fields.float(digits=(16,2), string='Nouveau Coeff.', required=True),
         'category_id': fields.many2one('product.category', string='Catégrorie'),
+        'type': fields.selection([('depart', 'Coeff. départ'), ('blanche', 'Coeff. Promo')], string='Coefficient', required=True),
     }
 
     _order = "name, coeff_new"
@@ -62,10 +71,14 @@ class wizard_modif_coeff(osv.osv_memory):
     def write(self, cr, uid, ids, data, context={}):
         product_obj = self.pool.get('product.product')
 
-        if 'coeff_new' in data:
+        if 'coeff_new' in data and 'type' in context:
             for coeff in self.browse(cr, uid, ids):
-                product_ids = product_obj.search(cr, uid, [('categ_id', '=', coeff.category_id.id), ('coeff_depart', '=', coeff.name)])
-                product_obj.write(cr, uid, product_ids, {'coeff_depart': data.get('coeff_new', coeff.name)}, context=context)
+                if context.get('type', False) == 'depart':
+                    product_ids = product_obj.search(cr, uid, [('categ_id', '=', coeff.category_id.id), ('coeff_depart', '=', coeff.name)])
+                    product_obj.write(cr, uid, product_ids, {'coeff_depart': data.get('coeff_new', coeff.name)}, context=context)
+                elif context.get('type', False) == 'blanche':
+                    product_ids = product_obj.search(cr, uid, [('categ_id', '=', coeff.category_id.id), ('coeff_blanche', '=', coeff.name)])
+                    product_obj.write(cr, uid, product_ids, {'coeff_blanche': data.get('coeff_new', coeff.name)}, context=context)
 
         return super(wizard_modif_coeff, self).write(cr, uid, ids, data, context=context)
 
