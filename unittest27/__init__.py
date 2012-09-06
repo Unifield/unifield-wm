d0bb7b76c788aa7986cf30bbcb5a83f9b5c13ef9
Samus CTO <cto@openerp.com>
2012-09-06 15:12:05 +0200

    [IMPORT] Use the original lib unittest from Python 2.7 and fix dependance problems

--- unittest27/__init__.py
+++ unittest27/__init__.py
@@ -1,64 +1,69 @@
-from unittest import *
+"""
+Python unit testing framework, based on Erich Gamma's JUnit and Kent Beck's
+Smalltalk testing framework.
 
-import unittest
-import types
-import sys
-import os
+This module contains the core framework classes that form the basis of
+specific test cases and suites (TestCase, TestSuite etc.), and also a
+text-based utility class for running the tests and reporting the results
+ (TextTestRunner).
 
+Simple usage:
+
+    import unittest
+
+    class IntegerArithmenticTestCase(unittest.TestCase):
+        def testAdd(self):  ## test method names begin 'test*'
+            self.assertEqual((1 + 2), 3)
+            self.assertEqual(0 + 1, 1)
+        def testMultiply(self):
+            self.assertEqual((0 * 10), 0)
+            self.assertEqual((5 * 8), 40)
+
+    if __name__ == '__main__':
+        unittest.main()
+
+Further information is available in the bundled documentation, and from
+
+  http://docs.python.org/library/unittest.html
+
+Copyright (c) 1999-2003 Steve Purcell
+Copyright (c) 2003-2010 Python Software Foundation
+This module is free software, and you may redistribute it and/or modify
+it under the same terms as Python itself, so long as this copyright message
+and disclaimer are retained in their original form.
+
+IN NO EVENT SHALL THE AUTHOR BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OF
+THIS CODE, EVEN IF THE AUTHOR HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH
+DAMAGE.
+
+THE AUTHOR SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE.  THE CODE PROVIDED HEREUNDER IS ON AN "AS IS" BASIS,
+AND THERE IS NO OBLIGATION WHATSOEVER TO PROVIDE MAINTENANCE,
+SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+"""
+
+__all__ = ['TestResult', 'TestCase', 'TestSuite',
+           'TextTestRunner', 'TestLoader', 'FunctionTestCase', 'main',
+           'defaultTestLoader', 'SkipTest', 'skip', 'skipIf', 'skipUnless',
+           'expectedFailure', 'TextTestResult', 'installHandler',
+           'registerResult', 'removeResult', 'removeHandler']
+
+# Expose obsolete functions for backwards compatibility
+__all__.extend(['getTestCaseNames', 'makeSuite', 'findTestCases'])
+
+__unittest = True
+
+from .result import TestResult
 from .case import (TestCase, FunctionTestCase, SkipTest, skip, skipIf,
                    skipUnless, expectedFailure)
-from .signals import installHandler
-import loader
-import runner
+from .suite import BaseTestSuite, TestSuite
+from .loader import (TestLoader, defaultTestLoader, makeSuite, getTestCaseNames,
+                     findTestCases)
+from .main import TestProgram, main
+from .runner import TextTestRunner, TextTestResult
+from .signals import installHandler, registerResult, removeResult, removeHandler
 
-import pdb
-
-def __init27__(self, module='__main__', defaultTest=None, argv=None,
-                testRunner=None, testLoader=loader.defaultTestLoader,
-                exit=True, verbosity=1, failfast=None, catchbreak=None,
-                buffer=None):
-    if isinstance(module, basestring):
-        self.module = __import__(module)
-        for part in module.split('.')[1:]:
-            self.module = getattr(self.module, part)
-    else:
-        self.module = module
-    if argv is None:
-        argv = sys.argv
-
-    self.exit = exit
-    self.failfast = failfast
-    self.catchbreak = catchbreak
-    self.verbosity = verbosity
-    self.buffer = buffer
-    self.defaultTest = defaultTest
-    self.testRunner = testRunner
-    self.testLoader = testLoader
-    self.progName = os.path.basename(argv[0])
-    self.parseArgs(argv)
-    self.runTests()
-
-unittest.TestProgram.__init__ = __init27__
-
-def runTests27(self):
-    if self.catchbreak:
-        installHandler()
-    if self.testRunner is None:
-        self.testRunner = runner.TextTestRunner
-    if isinstance(self.testRunner, (type, types.ClassType)):
-        try:
-            testRunner = self.testRunner(verbosity=self.verbosity,
-                                         failfast=self.failfast,
-                                         buffer=self.buffer)
-        except TypeError:
-            # didn't accept the verbosity, buffer or failfast arguments
-            testRunner = self.testRunner()
-    else:
-        # it is assumed to be a TestRunner instance
-        testRunner = self.testRunner
-    self.result = testRunner.run(self.test)
-    if self.exit:
-        sys.exit(not self.result.wasSuccessful())
-
-unittest.TestProgram.runTests = runTests27
-
+# deprecated
+_TextTestResult = TextTestResult
