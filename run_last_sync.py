ae3b75d7cedab3892631cdd1896eafc0233b3ae2
Samus CTO <cto@openerp.com>
2012-09-21 14:45:37 +0200

    [IMP] enable multiple db in run_last_sync.py

--- run_last_sync.py
+++ run_last_sync.py
@@ -2,7 +2,9 @@
 
 import sys
 
-from scripts.common import Synchro, HQ, Coordo, Project, Project2
+from scripts.common import db_instance, client, Synchro, HQ
+from config import project_count, coordo_count
+import config
 
 try:
     import ipdb as pdb
@@ -18,7 +20,36 @@ def sync(db):
         print 'Synchronization process of database "%s" failed!\n%s' % (db.db_name,monitor.read(ids, ['error'])[0]['error'])
 
 def sync_all():
-    for db in (HQ, Coordo, Project, Project2):
+    sync(HQ)
+    for i in range(1, coordo_count+1):
+        name = "%s_COORDO_%02d" % (config.prefix, i)
+        db = db_instance(
+            server=client,
+            name=name,
+            synchro={
+                'protocol' : 'netrpc',
+                'host' : config.server_host,
+                'port' : config.netrpc_port,
+                'database' : Synchro.name,
+                'login' : name,
+                'password' : name,
+            },
+        )
+        sync(db)
+    for i in range(1, project_count+1):
+        name = "%s_PROJECT_%02d" % (config.prefix, i)
+        db = db_instance(
+            server=client,
+            name=name,
+            synchro={
+                'protocol' : 'netrpc',
+                'host' : config.server_host,
+                'port' : config.netrpc_port,
+                'database' : Synchro.name,
+                'login' : name,
+                'password' : name,
+            },
+        )
         sync(db)
 
 if __name__ == '__main__':
