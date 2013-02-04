2dd6a8366cb28a9671722a1a702eb1b6851be41a
Xavier ALT <xal@openerp.com>
2013-02-04 10:29:26 +0100

    [AIO-45] update defaults branches to 'pilot', remove patches as integration to server/web branch required for AIO-45

--- package61_msf.py
+++ package61_msf.py
@@ -368,22 +368,10 @@ def options():
         return [ os.path.join(patches_dir, p) for p in patch_list ]
     o.web_patches = []
     o.server_patches = patches_list([
-        'win32_server60_logout.patch',
-        'win32_server60_python26.patch',
-        'win32_server60_lxml_external_entities.patch',
-        'all_server60_fix_test_disable_conffile_param.patch',
-        'win32_server60_fix_dbrestore.patch',
-        'win32_server60_setup_no_zip_archive.patch',
-        #'win32_server60_service_autorestart.patch',
     ])
     o.client_patches = patches_list([
-        'win32_gtk60_python26.patch'
     ])
     o.client_web_patches = patches_list([
-        'win32_web60_conffile.patch',
-        'win32_web60_logout.patch',
-        'win32_web60_python26.patch',
-        'win32_web60_setup_no_zip_archive.patch'
     ])
     return o
 
