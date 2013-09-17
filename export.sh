f53a9db8769b7c8d8b76134a89799e1698de6f59
Olivier DOSSMANN <od@tempo-consulting.fr>
2013-09-17 09:21:50 +0200

    [NEW] Scripts that create a synchronization environment, dump files and create an archive with dumps + some scripts to restore DB and launch an OpenERP server

---
+++ export.sh
@@ -0,0 +1,240 @@
+#!/usr/bin/env bash
+#
+# export.sh
+#
+# Create a synchronization environment and create an archive from it
+
+PROGRAM=`basename $0`
+VERSION="0.0.0"
+current_dir="$PWD"
+
+#####
+## Variables
+###
+
+# configuration files
+configfile="exportrc"
+mkdb_configfile="config.py"
+mkdb_tmp_configfile="config_temp.py"
+
+# official launchpad branches
+branch_server="lp:unifield-server"
+branch_addons="lp:unifield-addons"
+branch_web="lp:unifield-web"
+branch_wm="lp:unifield-wm"
+branch_sync="lp:unifield-wm/sync_module_prod"
+
+# temporary directory in which we will work
+tmpdir="${HOME}/tmp"
+
+# default HQ/PROJECT/COORDO counts
+hq_count=1
+coordo_count=2
+project_count=3
+
+#####
+## FUNCTIONS
+###
+
+error_and_exit() {
+  echo -e $1
+  exit 1
+}
+
+check_cmd_presence() {
+  cmd_path=`which $1`
+  if [ -z $cmd_path ]; then
+    error_and_exit "$1 is missing.\nInstall it: sudo apt-get update && sudo apt-get install $2"
+  fi
+  echo "$1: installed."
+}
+
+stop_server() {
+  start-stop-daemon --stop --quiet --pidfile $1 --oknodo
+}
+
+stop_server_and_exit() {
+  stop_server "$1" && exit 1
+}
+
+#####
+## TESTS
+###
+
+# Configuration file exists and is readable
+if ! [ -r $configfile ] ; then
+  error_and_exit "${configfile}: missing"
+fi
+# Load configuration file
+source $configfile
+echo "${configfile}: imported"
+
+# Check configuration file for MKDB script
+if ! [ -a $mkdb_configfile ] ; then
+  if ! [ -a $mkdb_tmp_configfile ] ; then
+    error_and_exit "${mkdb_tmp_configfile}: missing"
+  fi
+  # create mkdb configuration file
+  cp ${mkdb_tmp_configfile} ${mkdb_configfile}
+fi
+
+# Check some programs presence
+# pip
+# bzr
+# start-stop-daemon
+# tar
+# xz
+# perl
+# psql
+# netstat
+for cmd in "pip@pip" "bzr@bzr" "start-stop-daemon@dpkg" "tar@tar" "xz@xz-utils" "perl@perl-base" "psql@postgresql-client-common" "netstat@net-tools"; do
+  cmdname=`echo $cmd|cut -d '@' -f 1`
+  pkgname=`echo $cmd|cut -d '@' -f 2`
+  check_cmd_presence $cmdname $pkgname
+done
+
+# openerp-client-lib 1.0.1 presence
+if ! [ `pip search openerp-client-lib|grep INSTALLED|cut -d ':' -f 2` == '1.0.1' ] ; then
+  error_and_exit "openerp-client-lib 1.0.1 missing.\nInstall it: sudo pip install openerp-client-lib==1.0.1"
+fi
+echo "openerp-client-lib 1.0.1: installed."
+
+# tmp directory presence. If not create it.
+if [ -a "$tmpdir" ] ; then
+  if ! [ -d "$tmpdir" ] ; then
+    error_and_exit "$tmpdir exists but is not a directory. Change tmpdir variable."
+  fi
+else
+  mkdir "$tmpdir" || exit 1
+fi
+echo "Temp. dir: $tmpdir"
+
+#####
+## MAIN
+###
+
+## Fetch all branches (either from local or from remote)
+for branch in "web@${branch_web}@${web}" "server@${branch_server}@${server}" "addons@${branch_addons}@${addons}" "wm@${branch_wm}@${wm}" "sync@${branch_sync}@${sync}"; do
+  # prepare the transaction
+  copy=`echo $branch|cut -d '@' -f 1`
+  remote=`echo $branch|cut -d '@' -f 2`
+  inlocal=`echo $branch|cut -d '@' -f 3`
+  origin="$inlocal"
+  if ! [ -d "$inlocal" ]; then
+    origin="$remote"
+  fi
+  # Check if destination directory exists
+  # if not: fetch branch content either from local or from remote (regarding local existence)
+  if ! [ -d "${tmpdir}/${copy}" ]; then
+    echo -n "Fetching '$copy' from '$origin': "
+    bzr checkout -q --lightweight "$origin" "${tmpdir}/${copy}"
+    echo "DONE."
+  # if exists: update branch
+  else
+    cd ${tmpdir}/${copy}
+    echo -n "Updating '$copy': "
+    bzr pull -q
+    cd $current_dir
+    echo "DONE."
+  fi
+done
+
+## Check which port to use
+read XMLRPCPORT NETRPCPORT WEBPORT <<<`netstat -anltp 2> /dev/null | perl -e '%port = ();
+($min, $max) = (8100, 8200);
+while(<>) {
+  $port{$&} = 1 if m/:\K\d+\b/ and $& >= $min and $& <= $max;
+}
+for my $i ($min..$max) {
+  if( not exists $port{$i} and
+    not exists $port{$i+1} and
+    not exists $port{$i+2}) {
+    print join(" ", $i, $i+1, $i+2);
+    last;
+  }
+}'`
+echo "xmlrpc port: ${XMLRPCPORT}"
+echo "netrpc port: ${NETRPCPORT}"
+echo "web port: ${WEBPORT}"
+
+## Check that db is available
+db_exists=`psql -l|grep "${dbname}_*"|wc -l`
+while [ $db_exists -ne 0 ] ; do
+  dbname="${dbname}_1"
+  db_exists=`psql -l|grep "${dbname}_*"|wc -l`
+done
+echo "database: ${dbname}"
+
+## Change MKDB configuration file 'prefix' variable
+sed -i "s#\(prefix =\).*#\1 '${dbname}'#g" ${mkdb_configfile}
+## Same thing with different ports for server and web
+sed -i "s#\(client_port =\).*#\1 ${XMLRPCPORT}#g" ${mkdb_configfile}
+sed -i "s#\(server_port =\).*#\1 ${XMLRPCPORT}#g" ${mkdb_configfile}
+sed -i "s#\(netrpc_port =\).*#\1 ${NETRPCPORT}#g" ${mkdb_configfile}
+## Set HQ to 1, COORDO to 2 and PROJECT to 3
+sed -i "s#\(hq_count =\).*#\1 ${hq_count}#g" ${mkdb_configfile}
+sed -i "s#\(coordo_count =\).*#\1 ${coordo_count}#g" ${mkdb_configfile}
+sed -i "s#\(project_count =\).*#\1 ${project_count}#g" ${mkdb_configfile}
+
+## Launch server
+pidfile="${tmpdir}/${dbname}.pid"
+echo -n "Launching openerp-server: "
+start-stop-daemon --start --quiet --pidfile ${pidfile} \
+  --background --make-pidfile --exec ${tmpdir}/server/bin/openerp-server.py -- \
+  --no-xmlrpcs --netrpc-port=${NETRPCPORT} --xmlrpc-port=${XMLRPCPORT} \
+  --addons-path=${tmpdir}/addons,${tmpdir}/wm,${tmpdir}/sync
+# Wait 15 second before launching MKDB script
+sleep 15s
+echo "DONE."
+
+## Launch MKDB script
+cd ${current_dir} && ./mkdb.py || stop_server_and_exit ${pidfile}
+
+## Stop server
+stop_server ${pidfile}
+
+## Save databases
+# create a list file containing all files to save
+list="${tmpdir}/${dbname}.list"
+if ! [ -a "$list" ] ; then
+  touch $list
+else
+  echo "" > $list
+fi
+# save each DB in a dump file, add it to a list to save and drop DB
+for db in `psql template1 -t -c "SELECT datname from pg_database where datname ilike '${dbname}_%';"`; do
+  dumpfile="${db}.dump"
+  if [ -a "$dumpfile" ] ; then
+    error_and_exit "$dumpfile already exists! Aborted."
+  fi
+  echo -e -n "Saving '$db' to $dumpfile: "
+  pg_dump -Fc $db > $dumpfile
+  echo "$dumpfile" >> ${list}
+  dropdb $db
+  echo "DONE."
+done
+
+## Add some file into list file to archive
+echo "manage_syncdb.sh" >> ${list}
+echo "manage_syncdbrc" >> ${list}
+
+## Create archive
+echo -n "Creating archive: "
+archive="`date +'%Y%m%d'`_${dbname}.tar.xz"
+tar cfJ $archive `cat $list|tr -s '\n' ' '` || error_and_exit "An error occured to create archive: $archive."
+echo "$archive DONE."
+
+## Delete all DB dumps
+rm -f *.dump
+
+########################
+## WORK IN PROGRESS
+#####
+
+## TODO: Update CSV files in sync_module_prod for data to be correct
+## TODO: update all accounts on all databases
+## TODO: open all periods from january to today
+
+##############
+
+exit 0
---
+++ manage_syncdb.sh
@@ -0,0 +1,223 @@
+#!/usr/bin/env bash
+#
+# manage_syncdb.sh
+#
+# Synchronization databases management
+#
+
+#####
+## VARIABLES
+###
+
+PROGRAM=`basename $0`
+VERSION="0.0.0"
+config="`basename ${PROGRAM} .sh`rc"
+current="$PWD"
+
+#####
+## FUNCTIONS
+###
+
+# display this program's usage
+usage() {
+  cat << EOF
+usage: $PROGRAM [command] [dbname]
+
+This script helps to manage synchro databases.
+
+COMMANDS:
+backup      backup all databases prefixed by <dbname>_
+restore     restore all databases prefixed by <dbname>_ with a '.dump' suffix
+drop        drop all databases prefixed by <dbname>_
+start       launch a openerp server (need a config file '${config}')
+stop        stop a openerp server
+<dbname>    prefix of database to use
+extract     do a drop, restore and start for the given dbname (after an \
+archive extraction, for an example)
+EOF
+}
+
+# display an error
+error() {
+  echo -e "$1"
+}
+
+# display an error and quit
+error_and_exit() {
+  error "$1"
+  exit 1
+}
+
+# check if configuration file exists
+check_config() {
+  if ! [ -f "${config}" ] ; then
+    error_and_exit "Configuration file not found: $config."
+  fi
+}
+
+# check if some db with this prefix exists
+check_db() {
+db_exists=`psql -l|grep "$1_*"|wc -l`
+if [ $db_exists -eq 0 ] ; then
+  error_and_exit "No database with '$1' prefix found!"
+fi
+}
+
+# backup all DB that have a prefix "${dbname}_"
+backup() {
+  check_db "$1"
+  for db in `psql template1 -t -c "SELECT datname from pg_database where datname ilike '${1}_%';"`; do
+    dumpfile="${db}.dump"
+    if [ -a "$dumpfile" ] ; then
+      error_and_exit "$dumpfile already exists! Aborted."
+    fi
+    echo -e -n "Saving '$db' to $dumpfile: "
+    pg_dump -Fc $db > $dumpfile
+    echo "DONE."
+  done
+}
+
+# restore all .dump file that have a prefix with "${dbname}_"
+restore() {
+  for file in `ls ${1}_*`; do
+    db=`basename $file .dump`
+    exists=`psql template1 -t -c "SELECT COUNT(datname) from pg_database WHERE datname = '$db'"`
+    if [ $exists == 1 ] ; then
+      echo -n "$db exists. Deleting: "
+      dropdb $db || error_and_exit "An error occured @DB deletion: $db"
+      echo "DONE."
+    fi
+    echo -e -n "Restoring $db: "
+    createdb $db && pg_restore -d $db $file >/dev/null 2>&1
+    echo "DONE."
+  done
+}
+
+# drop all DB that begins with the given prefix + _ char
+drop() {
+  check_db "$1"
+  for db in `psql template1 -t -c "SELECT datname from pg_database where datname ilike '${1}_%';"`; do
+    echo -n "Deleting $db: "
+    dropdb $db || error_and_exit "An error occured @DB deletion: $db"
+    echo "DONE."
+  done
+}
+
+# start a unifield server on free ports (between 8100 and 8200)
+start_serv() {
+  # check if configuration is here and right completed
+  check_config
+  source "$config"
+  for var in wm server addons web sync ; do
+    if [ -z ${!var} ] ; then
+      error_and_exit "Variable not found in configuration file ($config): ${var}."
+    fi
+    # check directories existence
+    if ! [ -d ${!var} ] ; then
+      error_and_exit "Directory not found (${var} variable in $config file): ${!var}"
+    fi
+  done
+  # search free ports
+  echo -n "Searching free ports to launch server: "
+  read XMLRPCPORT NETRPCPORT WEBPORT <<<`netstat -anltp 2> /dev/null | perl -e '%port = ();
+  ($min, $max) = (8100, 8200);
+  while(<>) {
+    $port{$&} = 1 if m/:\K\d+\b/ and $& >= $min and $& <= $max;
+  }
+  for my $i ($min..$max) {
+    if( not exists $port{$i} and
+      not exists $port{$i+1} and
+      not exists $port{$i+2}) {
+      print join(" ", $i, $i+1, $i+2);
+      last;
+    }
+  }'`
+  echo "DONE."
+  # Prepare files and directories
+  pidfile="${current}/tmp/$1.pid"
+  logfile="${current}/tmp/$1.log"
+  if [ -a "${current}/tmp" ] ; then
+    if ! [ -d "${current}/tmp" ] ; then
+      error_and_exit "Directory ${current}/tmp not found! Is that a file?"
+    fi
+  else
+    echo -n "Creating ${current}/tmp directory: "
+    mkdir ${current}/tmp
+    echo "DONE."
+  fi
+  touch ${logfile}
+  echo -n "Launching openerp-server: "
+  LANG=C start-stop-daemon --start --pidfile ${pidfile} \
+  --background --make-pidfile --exec ${server}/bin/openerp-server.py -- \
+  --addons-path=${addons},${wm},${sync} --additional-xml --without-demo=all \
+  --logfile=${logfile} \
+  --no-xmlrpcs --no-xmlrpc --netrpc-port=${NETRPCPORT}
+  # Wait 15 second before launching MKDB script
+  sleep 15s
+  echo "DONE."
+  echo -e "Ports used:\nxmlrpc: ${XMLRPCPORT}\nnetrpc: ${NETRPCPORT}"
+  echo "You can launch a web server using this command:"
+  echo "${web}/openerp-web.py --openerp-port=${NETRPCPORT}"
+}
+
+stop_serv() {
+  pidfile="${current}/tmp/$1.pid"
+  logfile="${current}/tmp/$1.log"
+  if ! [ -a "$pidfile" ] ; then
+    error_and_exit "PID file $pidfile not found!"
+  fi
+  echo "Stopping server for $1"
+  start-stop-daemon --stop --quiet --pidfile $pidfile --oknodo
+  # Delete some files (PID and log)
+  rm -f $pidfile $logfile
+}
+
+#####
+## TESTS
+###
+
+# We need 2 params at least
+if [ $# -lt 2 ] ; then
+  error "Need at least 2 parameters."
+  usage
+  exit 1
+fi
+
+prefix=$1
+command=$2
+
+#####
+## MAIN
+###
+
+# Check command
+case $command in 
+  backup)
+  backup "$prefix"
+  ;;
+  restore)
+  restore "$prefix"
+  ;;
+  drop)
+  drop "$prefix"
+  ;;
+  start)
+  start_serv "$prefix"
+  ;;
+  stop)
+  stop_serv "$prefix"
+  ;;
+  extract)
+  drop "$prefix" && restore "$prefix" && start_serv "$prefix"
+  ;;
+  help)
+  usage
+  ;;
+  *)
+  error "Command $command not found!"
+  usage
+  exit 1
+  ;;
+esac
+
+exit 0
