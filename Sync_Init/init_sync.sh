a815f32d8857d62c7e54e88bc3fb0d122d8dd94a
jf <jf@ubuntu>
2013-12-31 08:16:03 +0100

    [IMP] tcp/port attribution [IMP] check if home dir exists

--- Sync_Init/init_sync.sh
+++ Sync_Init/init_sync.sh
@@ -1,5 +1,23 @@
 #! /bin/bash
 
+# directory to store home directory (do NOT add / at the end)
+TARGET_HOME_DIR="/home/SyncEnv"
+
+# Directory to store RB ports, no trailing slash
+BRANCHES="${TARGET_HOME_DIR}/BranchesList"
+
+# 3 ranges of 1000 tcp ports should be reserved to manage 999 RB
+# netstat is not used anymore
+# files in $BRANCHES are greped to find unused port
+NETRPC_INTV=53
+XMLRP_INTV=54
+WEB_INTV=55
+# Nb of tries to found a free port
+MAXTRIES=200
+
+# admin password to manage db from the web interface
+ADMINDBPASS="4unifield"
+
 BRANCH_DEFAULT_SERVER="lp:unifield-server"
 BRANCH_DEFAULT_ADDONS="lp:unifield-addons"
 BRANCH_DEFAULT_WEB="lp:unifield-web"
@@ -9,23 +27,24 @@ BRANCH_DEFAULT_ENV="lp:~unifield-team/unifield-wm/sync-env"
 
 APACHE_PREFIX="xxx_syncenv"
 APACHE_SITE_AVAILABLE="/etc/apache2/sites-available/syncenv"
-bzr_type=branch
+BZR_TYPE=branch
+#BZR_TYPE=checkout --lightweight
 
 # @ linux user creation copy some files / dir (used by bzr and homere import)
 FROM_DIR_TO_COPY="/opt/tools/runbot"
 HOME_TO_COPY="${FROM_DIR_TO_COPY}/.ssh/ ${FROM_DIR_TO_COPY}/.bazaar/ ${FROM_DIR_TO_COPY}/common/.bzr ${FROM_DIR_TO_COPY}/tmp/"
 
-# directory to store home directory (do NOT add / at the end)
-TARGET_HOME_DIR="/home/SyncEnv"
 
-#bzr_type=checkout --lightweight
 
 REV="$1"
-[ -z "$REV" ] && echo "Please specify revision: dsp-utp141 for example" && exit 1
-BRANCHES="branches/$REV"
+TAG=""
+[ -z "$REV" ] && echo "Please specify revision: dsp-utp141 for example, optionally followed by a bzr tag" && exit 1
+[ -n "$2" ] && TAG="-r $2 "
+[ ! -d ${BRANCHES} ] && mkdir -m 700 -p $BRANCHES
+BRANCHE_INFO="${BRANCHES}/${REV}"
 
-if [ -f "$BRANCHES" ]; then
-    . "$BRANCHES"
+if [ -f "${BRANCHE_INFO}" ]; then
+    . "$BRANCHE_INFO"
     correct=skip
 else
     correct=no
@@ -35,16 +54,16 @@ while ! [ $correct == "y" ]
 do
     echo "To get a specific tag or revno use the following notation: '-r revno lp:your_branch'"
     if ! [ "$correct" == "skip" ]; then
-        echo -n "Enter server branch [$BRANCH_DEFAULT_SERVER]: "; read server
-        [ -z "$server" ] && server=$BRANCH_DEFAULT_SERVER
-        echo -n "Enter addons branch [$BRANCH_DEFAULT_ADDONS]: "; read addons
-        [ -z "$addons" ] && addons=$BRANCH_DEFAULT_ADDONS
-        echo -n "Enter web branch [$BRANCH_DEFAULT_WEB]: "; read web
-        [ -z "$web" ] && web=$BRANCH_DEFAULT_WEB
-        echo -n "Enter wm branch [$BRANCH_DEFAULT_WM]: "; read wm
-        [ -z "$wm" ] && wm=$BRANCH_DEFAULT_WM
-        echo -n "Enter sync branch [$BRANCH_DEFAULT_SYNC]: "; read sync
-        [ -z "$sync" ] && sync=$BRANCH_DEFAULT_SYNC
+        echo -n "Enter server branch [${TAG}${BRANCH_DEFAULT_SERVER}]: "; read server
+        [ -z "$server" ] && server="${TAG}${BRANCH_DEFAULT_SERVER}"
+        echo -n "Enter addons branch [${TAG}${BRANCH_DEFAULT_ADDONS}]: "; read addons
+        [ -z "$addons" ] && addons="${TAG}$BRANCH_DEFAULT_ADDONS"
+        echo -n "Enter web branch [${TAG}$BRANCH_DEFAULT_WEB]: "; read web
+        [ -z "$web" ] && web="${TAG}$BRANCH_DEFAULT_WEB"
+        echo -n "Enter wm branch [${TAG}$BRANCH_DEFAULT_WM]: "; read wm
+        [ -z "$wm" ] && wm="${TAG}$BRANCH_DEFAULT_WM"
+        echo -n "Enter sync branch [${TAG}$BRANCH_DEFAULT_SYNC]: "; read sync
+        [ -z "$sync" ] && sync="${TAG}$BRANCH_DEFAULT_SYNC"
         echo -n "Enter env branch [$BRANCH_DEFAULT_ENV]: "; read env
         [ -z "$env" ] && env=$BRANCH_DEFAULT_ENV
     fi
@@ -59,26 +78,39 @@ do
     [ -z "$correct" ] && correct=y
 done
 
-echo "server=\"$server\"" > $BRANCHES
-echo "addons=\"$addons\"" >> $BRANCHES
-echo "web=\"$web\"" >> $BRANCHES
-echo "wm=\"$wm\"" >> $BRANCHES
-echo "sync=\"$sync\"" >> $BRANCHES
-echo "env=\"$env\"" >> $BRANCHES
+FOUND=0
+# TODO LOCK FILE
+if [[ -n ${NETRPCPORT} ]]; then
+    echo "Netrpcport ${NETRPCPORT} defined in $BRANCHE_INFO"
+    let FOUND=$MAXTRIES+1
 
-read NETRPCPORT WEBPORT XMLRPCPORT <<<`netstat -anltp 2> /dev/null | perl -e '%port = ();
-($min, $max) = (51200, 51999);
-while(<>) {
-    $port{$&} = 1 if m/:\K\d+\b/ and $& >= $min and $& <= $max;
-}
-for my $i ($min..$max) {
-    if( not exists $port{$i} and
-        not exists $port{$i+1} and
-        not exists $port{$i+2}) {
-            print join(" ", $i, $i+1, $i+2);
-            last;
-    }
-}'`
+while [[ ${FOUND} -lt ${MAXTRIES} ]]; do
+    NETRPCPORT=`shuf -i ${NETRPC_INTV}000-${NETRPC_INTV}999 -n 1`
+    echo $NETRPCSTRING ${FOUND} ${MAXTRIES}
+    let FOUND=${FOUND}+1
+    if ! grep -xq "NETRPCPORT=${NETRPCPORT}" $BRANCHES; then
+        let FOUND=MAXTRIES+1
+    fi
+done
+
+if [[ ${FOUND} -eq ${MAXTRIES} ]]; then
+   echo "Can't find a free port after ${MAXTRIES} tries ! Script Aborted"
+   echo "If you feel lucky you can restart the script"
+   exit 0
+fi
+
+echo "NETRPCPORT=${NETRPCPORT}" > $BRANCHE_INFO
+echo "server=\"$server\"" >> $BRANCHE_INFO
+echo "addons=\"$addons\"" >> $BRANCHE_INFO
+echo "web=\"$web\"" >> $BRANCHE_INFO
+echo "wm=\"$wm\"" >> $BRANCHE_INFO
+echo "sync=\"$sync\"" >> $BRANCHE_INFO
+echo "env=\"$env\"" >> $BRANCHE_INFO
+let XMLRPCPORT=${XMLRP_INTV}${NETRPCPORT:2}
+let WEBPORT=${WEB_INTV}${NETRPCPORT:2}
+
+#echo "netrpc: ${NETRPCPORT}, xmlrpc: ${XMLRPCPORT}, web: ${WEBPORT}"
+
 URL="http://$REV.dsp.uf3.unifield.org:$WEBPORT"
 
 USERERP=${REV}
@@ -86,12 +118,18 @@ USERERP_HOME="${TARGET_HOME_DIR}/${REV}"
 APACHEPORT="80"
 APACHEHOST=${REV}
 DBNAME="${REV}"
-ADMINDBPASS="4unifield"
 
+user_creation=0
 check_init() {
     if [ -d ${USERERP_HOME} ]; then
-        echo "${USERERP_HOME} exists ! Abording ..."
-        exit 1
+        read -n 1 -p "Home dir exists, enter return to bypass user creation, or enter any non white space char to abort " user_creation
+        if [ -n "$user_creation" ]; then
+            echo ""
+            echo "${USERERP_HOME} exists ! Aborting ..."
+            exit 1
+        else
+            user_creation=1
+        fi
     fi
     if [ ! -d ${TARGET_HOME_DIR} ]; then
         mkdir -m 755 -p ${TARGET_HOME_DIR}
@@ -120,7 +158,7 @@ config_file() {
     create_file ./File/apache.conf ${APACHE_SITE_AVAILABLE}/${USERERP}
     create_file ./File/sync-env.py ${USERERP_HOME}/sync_env_script/config.py
 
-    ln -sv "../sites-available/${USERERP}" "/etc/apache2/sites-enabled/${APACHE_PREFIX}-${USERERP}"
+    ln -sv "${APACHE_SITE_AVAILABLE}/${USERERP}" "/etc/apache2/sites-enabled/${APACHE_PREFIX}-${USERERP}"
     chown ${USERERP}.${USERERP} ${USERERP_HOME}/etc/openerp-web.cfg ${USERERP_HOME}/etc/openerprc ${USERERP_HOME}/sync_env_script/config.py
     update-rc.d ${USERERP}-web defaults
     update-rc.d ${USERERP}-server defaults
@@ -128,24 +166,24 @@ config_file() {
 }
 
 init_user() {
-    useradd -s /bin/bash -d ${USERERP_HOME} -m ${USERERP}
+    if [ ${user_creation} -eq 0 ]; then
+        useradd -s /bin/bash -d ${USERERP_HOME} -m ${USERERP}
+        for to_copy in ${HOME_TO_COPY}; do
+            cp -a ${to_copy}/ ${USERERP_HOME}
+        done
+    fi
     su - postgres -c -- "createuser -S -R -d ${USERERP}"
-    for to_copy in ${HOME_TO_COPY}; do
-        cp -a ${to_copy}/ ${USERERP_HOME}
-    done
     chown -R ${USERERP}.${USERERP} ${USERERP_HOME}
     su - ${USERERP} <<EOF
 
-bzr ${bzr_type} "${wm:=${BRANCH_DEFAULT_WM}}" unifield-wm
-bzr ${bzr_type} "${addons:=${BRANCH_DEFAULT_ADDONS}}" unifield-addons
-bzr ${bzr_type} "${web:=${BRANCH_DEFAULT_WEB}}" unifield-web
-bzr ${bzr_type} "${server:=${BRANCH_DEFAULT_SERVER}}" unifield-server
-bzr ${bzr_type} "${sync:=${BRANCH_DEFAULT_SYNC}}" sync_module_prod
-bzr ${bzr_type} "${env:=${BRANCH_DEFAULT_ENV}}" sync_env_script
+bzr ${BZR_TYPE} ${wm:=${BRANCH_DEFAULT_WM}} unifield-wm
+bzr ${BZR_TYPE} ${addons:=${BRANCH_DEFAULT_ADDONS}} unifield-addons
+bzr ${BZR_TYPE} ${web:=${BRANCH_DEFAULT_WEB}} unifield-web
+bzr ${BZR_TYPE} ${server:=${BRANCH_DEFAULT_SERVER}} unifield-server
+bzr ${BZR_TYPE} ${sync:=${BRANCH_DEFAULT_SYNC}} sync_module_prod
+bzr ${BZR_TYPE} ${env:=${BRANCH_DEFAULT_ENV}} sync_env_script
 
 mkdir etc log exports
-#createdb ${DBNAME}
-
 echo Configure http://${REV}.dsp.uf3.unifield.org/
 EOF
 }
