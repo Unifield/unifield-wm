046afb0fb183f5489075eec0ff31917ee0c39bd4
Olivier DOSSMANN <od@tempo-consulting.fr>
2014-05-09 12:13:07 +0200

    UF-1746 [FIX] Bug on analytic lines that are corrected in closed period: No REV and COR lines.
    This is due to the UF-1746 branch in which the "def corrected_upstream" make a write on account move line which delete and recreate analytic lines.
    
    As this write permits to indicate synchronization that a change was done, I use "update_check=False, check=False" to avoid analytic lines deletion/recreation. Which solves the problem.

--- account_corrections/account_move_line.py
+++ account_corrections/account_move_line.py
@@ -886,7 +886,7 @@ receivable, item have not been corrected, item have not been reversed and accoun
             # UF-1746: Set also all other move lines as corrected upstream to disallow projet user to correct any move line of this move.
             move_ids = [x and x.get('move_id', False) and x.get('move_id')[0] for x in self.read(cr, uid, ids, ['move_id'], context=context)]
             ml_ids = self.search(cr, uid, [('move_id', 'in', move_ids)])
-            self.write(cr, uid, ml_ids, {'corrected_upstream': True}, context=context)
+            self.write(cr, uid, ml_ids, {'corrected_upstream': True}, check=False, update_check=False, context=context)
         return True
 
 account_move_line()
