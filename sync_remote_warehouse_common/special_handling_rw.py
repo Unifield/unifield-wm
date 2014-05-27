82537d9aef3d61e0a0dceefe51a5e7e35e679a03
Cecile Tonglet <cto@openerp.com>
2014-05-27 13:23:54 +0200

    [FIX] location_requestor_rw is now a m2o

--- sync_remote_warehouse_common/special_handling_rw.py
+++ sync_remote_warehouse_common/special_handling_rw.py
@@ -35,10 +35,9 @@ from tools.translate import _
 class stock_move(osv.osv):
     # This is to treat the location requestor on Remote warehouse instance if IN comes from an IR
     _inherit = 'stock.move'
-    _columns = {'location_requestor_rw': fields.integer(string='Location Requestor For RW-IR'),
-                }
-    _defaults = {
-        'location_requestor_rw': -1,
+    _columns = {
+        'location_requestor_rw':
+            fields.many2one('stock.location', 'Location Requestor For RW-IR'),
     }
 
     def create(self, cr, uid, vals, context=None):
@@ -66,8 +65,8 @@ class stock_move(osv.osv):
         type = self.pool.get('sync.client.entity').get_entity(cr, uid).usb_instance_type
         if type == 'remote_warehouse':
             move = kwargs['move']
-            if move.location_requestor_rw != -1:
-                return move.location_requestor_rw
+            if move.location_requestor_rw:
+                return move.location_requestor_rw.id
         # for any case, just return False and let the caller to pick the normal loc requestor
         return False
 
