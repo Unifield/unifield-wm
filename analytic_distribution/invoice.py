1447a15dd052f4ffaebd102564f40c23c897f885
Olivier DOSSMANN <olivier+launchpad@dossmann.net>
2014-09-29 15:20:45 +0200

    UTP-594 [ADD] Link to the previous invoice when we are in a refund

--- analytic_distribution/invoice.py
+++ analytic_distribution/invoice.py
@@ -143,15 +143,26 @@ class account_invoice(osv.osv):
         """
         Reverse lines for given invoice
         """
+        # Prepare some values
         if isinstance(ids, (int, long)):
             ids = [ids]
+        new_ids = []
+        # Process source invoices
         for inv in self.browse(cr, uid, ids):
             # Check for dates (refund must be done after invoice)
             if date and date < inv.date_invoice:
                 raise osv.except_osv(_('Error'), _("Posting date for the refund is before the invoice's posting date!"))
             if document_date and document_date < inv.document_date:
                 raise osv.except_osv(_('Error'), _("Document date for the refund is before the invoice's document date!"))
-        new_ids = super(account_invoice, self).refund(cr, uid, ids, date, period_id, description, journal_id)
+            new_id = super(account_invoice, self).refund(cr, uid, inv.id date, period_id, description, journal_id)
+            # UTP-594: Add Source Document on new invoice (origin field) and a link to the original invoice
+            self.write(cr, uid, new_id, {
+                'origin': inv.number or '',
+                'refund_source_inv_id': inv.id,
+            })
+            if isinstance(new_id, (int, long)):
+                new_id = [new_id]
+            new_ids += new_id
         # add document date
         if document_date:
             self.write(cr, uid, new_ids, {'document_date': document_date})
