8facc3cf5478b3c8bae4ee4806eb350b16d6cd48
jf <jfb@tempo-consulting.fr>
2014-10-10 11:06:09 +0200

    UF-2506 [FIX] Duplication of account_destination_link on project after sync

--- analytic_distribution/account.py
+++ analytic_distribution/account.py
@@ -222,7 +222,7 @@ class account_account(osv.osv):
         if context is None:
             context = {}
         # Add default_destination_id in destination_ids if exists
-        if 'default_destination_id' in vals and vals.get('default_destination_id', False):
+        if not context.get('sync_update_execution') and 'default_destination_id' in vals and vals.get('default_destination_id', False):
             vals.update({'destination_ids': [(4, vals.get('default_destination_id'))]})
         return super(account_account, self).create(cr, uid, vals, context=context)
 
