bf83d2cbb0b0e90321d23961f901b4edc979dd97
Olivier DOSSMANN <olivier+launchpad@dossmann.net>
2014-11-18 12:00:48 +0100

    [TEST] Add analytic account creation check

--- unifield_tests/tests/test_0010_account.py
+++ unifield_tests/tests/test_0010_account.py
@@ -1,15 +1,52 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 from unifield_test import UnifieldTest
+from oerplib.error import RPCError
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 class AccountTest(UnifieldTest):
 
+    def setUp(self):
+        '''
+        Initialize some accounting objects
+        '''
+        self.ana_account_to_delete = []
+        db = self.p1
+        self.acc_obj = db.get('account.account')
+        self.ana_obj = db.get('account.analytic.account')
+
+    def tearDown(self):
+        '''
+        Clean up database
+        '''
+        if self.ana_account_to_delete:
+            self.ana_obj.unlink(self.ana_account_to_delete)
+
     def test_010_coa(self):
         '''Check Chart of Account length'''
-        ids = self.p1.get('account.account').search([])
+        ids = self.acc_obj.search([])
         self.assert_(len(ids) == 357, "Chart of Account length: %s" % len(ids))
 
-    def test_020_acoa(self):
+    def test_020_analytic_creation(self):
+        '''Check activation date is 3 month ago for ANA account'''
+        nextDate = '%s-01-01' % (datetime.now().year + 2)
+        vals = {
+            'name': 'Test Account',
+            'date': nextDate,
+        }
+        try:
+            ana_id = self.ana_obj.create(vals)
+        except RPCError, e:
+            raise Exception("%s\n\n%s", (e.message, e.oerp_traceback))
+        # Do not forget to delete this account after tests
+        self.ana_account_to_delete.append(ana_id)
+        # Check account data after creation process
+        account = self.ana_obj.browse(ana_id)
+        previousDate = (datetime.today() + relativedelta(months=-3)).strftime('%Y-%m-%d')
+        self.assert_(str(account.date_start) == previousDate, "Wrong date: %s (%s). Should be: %s (%s)" % (account.date_start, type(account.date_start), previousDate, type(previousDate)))
+
+    def test_100_acoa(self):
         '''Print Analytic Chart of Account through the wizard'''
         # Get report object
         wiz_obj = self.p1.get('account.analytic.chart')
