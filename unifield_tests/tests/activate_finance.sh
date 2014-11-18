95df98b562dd2b55f5ae8a8f1f1738f1cda151a6
Olivier DOSSMANN <olivier+launchpad@dossmann.net>
2014-11-18 10:38:54 +0100

    [TEST] Add new test for analytic chart of account

---
+++ unifield_tests/tests/activate_finance.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+for i in `cat to_deactivate_for_finance_tests.list`; do echo mv $i deactivated/; done
--- unifield_tests/tests/test_0010_account.py
+++ unifield_tests/tests/test_0010_account.py
@@ -9,6 +9,18 @@ class AccountTest(UnifieldTest):
         ids = self.p1.get('account.account').search([])
         self.assert_(len(ids) == 357, "Chart of Account length: %s" % len(ids))
 
+    def test_020_acoa(self):
+        '''Print Analytic Chart of Account through the wizard'''
+        # Get report object
+        wiz_obj = self.p1.get('account.analytic.chart')
+        wiz_id = wiz_obj.create({ 'show_inactive': True, }, {})
+        res = wiz_obj.button_export([wiz_id], {})
+        # Check wizard result
+        self.assert_(res.get('type', False) == 'ir.actions.report.xml', "Wrong report type")
+        # Launch report
+        # TODO: find a way to launch a report
+#        report_res = self.p1.report('account_analytic_chart_export', 'account.analytic.account', res.get('datas').get('ids'), 'webkit', {'show_inactive': True})
+
 def get_test_class():
     '''Return the class to use for tests'''
     return AccountTest
