61eaa72310baf71aefbf296b6ef50899c1655cd6
Quentin THEURET <qt@tempo-consulting.fr>
2014-11-21 16:34:05 +0100

    [ADD] Gestion des reliquats

---
+++ server/bin/addons/iller_stock/__terp__.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+#-*- encoding:utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution    
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    Tempo Consulting (<http://www.tempo-consulting.fr/>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Gestion des stocks',
+    'version': '0.1',
+    'category': 'Generic Modules/Projects & Services',
+    'description': """
+        Ce module ajoute la gestion des reliquats pour les livraisons de Iller.
+""",
+    'author': 'TeMPO Consulting',
+    'website': 'http://www.tempo-consulting.fr',
+    'depends': [
+        'stock',
+    ],
+    'init_xml': [],
+    'demo_xml': [],
+    'update_xml': [
+        'stock_wizard.xml',
+        'stock_view.xml',
+        #'security/ir.model.access.csv',
+        #'security/iller_stock_security.xml',
+    ],
+    'installable': True,
+    'active': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
