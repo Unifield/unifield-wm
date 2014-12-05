025af021e8eda08c52878bc1ca40d31b5988dd41
Olivier DOSSMANN <olivier+launchpad@dossmann.net>
2014-12-05 10:38:23 +0100

    [TEST] Move register line creation method to be used by other Finance Tests

--- unifield_tests/tests/finance.py
+++ unifield_tests/tests/finance.py
@@ -3,6 +3,7 @@
 from __future__ import print_function
 from unifield_test import UnifieldTest
 from time import strftime
+from datetime import datetime
 from random import randint
 from oerplib import error
 
@@ -233,5 +234,47 @@ class FinanceTest(UnifieldTest):
             elif register.journal_id.type == 'cheque':
                 reg_obj.button_open_cheque([register.id])
         return True
+
+    def create_register_line(self, register_id, code, amount, date=False, document_date=False, third_partner_id=False, third_employee_id=False, third_journal_id=False):
+        """Create a register line with the given account code and amount. Optionnaly third party"""
+        # Check register_id presence
+        if not register_id:
+            raise Exception("Register ID is missing.")
+        # Prepare some values
+        description = ''
+        register = self.reg_obj.browse(register_id)
+        # Check account code
+        code_ids = self.a_obj.search(['|', ('name', 'ilike', code), ('code', 'ilike', code)])
+        if len(code_ids) != 1:
+            raise Exception("Error searching for this account code: %s. Need %s codes." % (code, len(code_ids) > 1 and 'less' or 'more'))
+        account_id = code_ids[0]
+        # Check dates
+        if not date:
+            date_start = register.period_id.date_start or False
+            date_stop = register.period_id.date_stop or False
+            if not date_start or not date_stop:
+                raise Exception("No date found for the period %s." % register.period_id.name)
+            random_date = self.random_date(datetime.strptime(str(date_start), '%Y-%m-%d'), datetime.strptime(str(date_stop), '%Y-%m-%d'))
+            date = datetime.strftime(random_date, '%Y-%m-%d')
+        if not document_date:
+            document_date = date
+        # Prepare some values
+        if not description:
+            description = self.random_word()
+        vals = {
+            'statement_id': register_id,
+            'account_id': account_id,
+            'document_date': document_date,
+            'date': date,
+            'amount': amount,
+            'name': description,
+        }
+        if third_partner_id:
+            vals.update({'partner_id': third_partner_id})
+        if third_employee_id:
+            vals.update({'employee_id': third_employee_id})
+        if third_journal_id:
+            vals.update({'transfer_journal_id': third_journal_id})
+        return self.absl_obj.create(vals)
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
