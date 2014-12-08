77d7c8b40c2d7f31f26d0dc122ee231f0382a153
Olivier DOSSMANN <olivier+launchpad@dossmann.net>
2014-12-08 17:29:56 +0100

    [TEST] Complete register line creation tests and ADD a new (work in progress) method that generates an analytic distribution

--- unifield_tests/tests/test_0040_registers.py
+++ unifield_tests/tests/test_0040_registers.py
@@ -3,6 +3,7 @@
 from oerplib.error import RPCError
 from finance import FinanceTest
 from datetime import datetime
+from random import randint
 
 class RegisterTest(FinanceTest):
 
@@ -34,7 +35,29 @@ class RegisterTest(FinanceTest):
     def test_010_direct_payment(self):
         """Direct payment: a register line with an expense account and WITHOUT 3RD PARTY."""
         # Create the direct payment
-        self.create_register_line(self.register_id, '63000', '333')
+        amount = -1 * randint(1, 10000)
+        line_id = self.create_register_line(self.register_id, '63000', amount)
+        if not line_id:
+            raise Exception("No register line created.")
+        # Some checks on it
+        line = self.absl_obj.browse(line_id)
+        # Check the line state (should be draft)
+        self.assert_(line.state == 'draft', "Wrong line state (ID: %s). Should be: %s. Current: %s." % (line_id, 'draft', line.state or ''))
+        # Check that no move line is attached
+        move_ids = [x.id for x in line.move_ids]
+        self.assert_(move_ids == [], "Move lines detected on line (ID: %s). Should not. Current: %s" % (line_id, move_ids))
+        # Attach a distribution analytic on it
+        distrib_id = self.generate_analytic_distribution(self.p1)
+        self.absl_obj.write([line.id], {'analytic_distribution_id': distrib_id}, {})
+        # Temp post the line
+        try:
+            self.absl_obj.posting([line.id], 'temp')
+        except RPCError, e:
+            raise Exception('ERROR WHILE %s POSTING LINE:\n%s\n\n%s' % ('temp', e.message, e.oerp_traceback))
+        # Check state, move lines presence and journal entry state
+        self.assert_(line.state == 'temp', "Wrong line state (ID: %s). Should be: %s. Current: %s" % (line_id, 'temp', line.state or ''))
+        move_ids = [x.id for x in line.move_ids]
+        self.assert_(move_ids != [], "No move lines detected on line (ID: %s)." % (line_id))
 
 def get_test_class():
     '''Return the class to use for tests'''
