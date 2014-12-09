378e184a47d8b98b593ab6a86f66a4f0c7ced9a9
Olivier DOSSMANN <olivier+launchpad@dossmann.net>
2014-12-09 15:08:01 +0100

    [TEST] Direct expense (that generates a journal entry with reconciled lines)

--- unifield_tests/tests/test_0040_registers.py
+++ unifield_tests/tests/test_0040_registers.py
@@ -65,8 +65,11 @@ class RegisterTest(FinanceTest):
         except RPCError as e:
             raise Exception('ERROR WHILE %s POSTING LINE:\n%s\n\n%s' % ('hard', e.message, e.oerp_traceback))
         line = self.absl_obj.browse(line_id) # need to browse register line to update record values
+        move_ids = [x.id for x in line.move_ids]
         # Check state, move lines presence and journal entry state
         self.assert_(line.state == 'hard', "Wrong line state (ID: %s). Should be: %s. Current: %s" % (line.id, 'hard', line.state))
+        for move in line.move_ids:
+            self.assert_(move.state == 'posted', "Register line (ID: %s) have journal entry (ID: %s) in wrong state. Should be posted. Current: %s." % (line.id, move.id, move.state))
 
     def test_020_direct_expense(self):
         """A direct expense is a register expense line with a supplier 3RD party that generates a supplementary Journal Entry"""
@@ -81,7 +84,34 @@ class RegisterTest(FinanceTest):
             raise Exception("No register line created.")
         # Some checks on it
         line = self.absl_obj.browse(line_id)
+        move_ids = [x.id for x in line.move_ids]
         self.assert_(line.state == 'draft', "Wrong line state (ID: %s). Should be: %s. Current: %s" % (line_id, 'draft', line.state or ''))
+        self.assert_(move_ids == [], "Move lines detected on line (ID: %s). Should not. Current: %s" % (line_id, move_ids))
+        # Temp post the line
+        try:
+            self.absl_obj.posting([line.id], 'temp')
+        except RPCError as e:
+            raise Exception('ERROR WHILE %s POSTING LINE:\n%s\n\n%s' % ('temp', e.message, e.oerp_traceback))
+        line = self.absl_obj.browse(line_id) # need to browse register line to update record values
+        move_ids = [x.id for x in line.move_ids]
+        # Check state, move lines presence and journal entry state
+        self.assert_(line.state == 'temp', "Wrong line state (ID: %s). Should be: %s. Current: %s" % (line_id, 'temp', line.state or ''))
+        # Hard post the line
+        try:
+            self.absl_obj.posting([line.id], 'hard')
+        except RPCError as e:
+            raise Exception('ERROR WHILE %s POSTING LINE:\n%s\n\n%s' % ('hard', e.message, e.oerp_traceback))
+        line = self.absl_obj.browse(line_id) # need to browse register line to update record values
+        move_ids = [x.id for x in line.move_ids]
+        # Check state, move lines presence and journal entry state
+        self.assert_(line.state == 'hard', "Wrong line state (ID: %s). Should be: %s. Current: %s" % (line.id, 'hard', line.state))
+        for move in line.move_ids:
+            self.assert_(move.state == 'posted', "Register line (ID: %s) have journal entry (ID: %s) in wrong state. Should be posted. Current: %s." % (line.id, move.id, move.state))
+        # Direct expense generates a second Journal Entry with same amount, partner and with reconciled lines. So we search if these lines exists.
+        # As the system is not well designed (with move_ids field in register lines) we have to make a search in move lines instead of using move_ids field that doesn't contain all linked move_ids.
+        move_line_ids = self.p1.get('account.move.line').search([('name', '=', line.name), ('statement_id', '=', line.statement_id.id), ('partner_id', '=', partner_id), ('is_reconciled', '=', True)])
+        self.assert_(move_line_ids != [], "No move lines found for the direct expense (ID: %s)" % line.id)
+        self.assert_(len(move_line_ids) == 2, "Problem occured for register line '%s' (ID: %s). Should find 2 reconciled journal items. Found: %s." % (line.name, line.id, len(move_line_ids)))
 
 def get_test_class():
     '''Return the class to use for tests'''
