e7ccab88a524c42278ddfb2548d73602fa84ebe1
Quentin THEURET <qt@tempo-consulting.fr>
2015-02-11 09:45:27 +0100

    SP-181 [IMP] Add INFOLOG message when the currency is changed in the FO

--- sale_override/wizard/order_change_currency.py
+++ sale_override/wizard/order_change_currency.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2011 TeMPO Consulting, MSF 
+#    Copyright (C) 2011 TeMPO Consulting, MSF
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -26,70 +26,124 @@ from tools.translate import _
 
 class sale_order_change_currency(osv.osv_memory):
     _name = 'sale.order.change.currency'
-    
+
     _columns = {
-        'order_id': fields.many2one('sale.order', string='Order', required=True),
-        'partner_id': fields.many2one('res.partner', string='Partner', required=True),
-        'partner_type': fields.selection(string='Partner Type', selection=PARTNER_TYPE),
-        'old_pricelist_id': fields.many2one('product.pricelist', string='Old currency', required=True, readonly=True),
-        'new_pricelist_id': fields.many2one('product.pricelist', string='New currency', required=True),
-        'currency_rate': fields.float(digits=(16,6), string='Currency rate', readonly=True),
+        'order_id': fields.many2one(
+            'sale.order',
+            string='Order',
+            required=True,
+        ),
+        'partner_id': fields.many2one(
+            'res.partner',
+            string='Partner',
+            required=True,
+        ),
+        'partner_type': fields.selection(
+            string='Partner Type',
+            selection=PARTNER_TYPE,
+        ),
+        'old_pricelist_id': fields.many2one(
+            'product.pricelist',
+            string='Old currency',
+            required=True,
+            readonly=True,
+        ),
+        'new_pricelist_id': fields.many2one(
+            'product.pricelist',
+            string='New currency',
+            required=True,
+        ),
+        'currency_rate': fields.float(
+            digits=(16, 6),
+            string='Currency rate',
+            readonly=True,
+        ),
     }
-    
-    def compute_pricelist_rate(self, cr, uid, from_pricelist, to_pricelist, context=None):
+
+    def compute_pricelist_rate(self, cr, uid,
+                               from_pricelist,
+                               to_pricelist, context=None):
         '''
         Compute the currency rate between two products pricelists
         '''
         if not from_pricelist and not to_pricelist:
-            raise osv.except_osv(_('Error'), _('Please give two pricelists'))
-        
+            raise osv.except_osv(
+                _('Error'),
+                _('Please give two pricelists')
+            )
+
         pricelist_obj = self.pool.get('product.pricelist')
-        from_currency = pricelist_obj.browse(cr, uid, from_pricelist).currency_id.id
-        to_currency = pricelist_obj.browse(cr, uid, to_pricelist).currency_id.id
-        
-        rate = self.pool.get('res.currency').compute(cr, uid, from_currency, to_currency, 1.00)
-        
+        cur_obj = self.pool.get('res.currency')
+
+        from_currency = pricelist_obj.browse(
+            cr, uid, from_pricelist).currency_id.id
+        to_currency = pricelist_obj.browse(
+            cr, uid, to_pricelist).currency_id.id
+
+        rate = cur_obj.compute(cr, uid, from_currency, to_currency, 1.00)
+
         return rate
-    
-    def currency_change(self, cr, uid, ids, old_pricelist_id, new_pricelist_id):
+
+    def currency_change(self, cr, uid, ids,
+                        old_pricelist_id,
+                        new_pricelist_id):
         '''
         Compute the rate between the two currencies
         '''
         res = {'currency_rate': 0.00}
         if old_pricelist_id and new_pricelist_id:
-            rate = self.compute_pricelist_rate(cr, uid, old_pricelist_id, new_pricelist_id)
+            rate = self.compute_pricelist_rate(
+                cr, uid, old_pricelist_id, new_pricelist_id)
             res = {'currency_rate': rate}
-            
+
         return {'value': res}
-    
+
     def apply_to_lines(self, cr, uid, ids, context=None):
         '''
         Apply the conversion on lines
         '''
         if not context:
             context = {}
-            
+
         if isinstance(ids, (int, long)):
             ids = [ids]
-            
+
         c = context.copy()
         c.update({'update_merge': True})
-            
+
         currency_obj = self.pool.get('res.currency')
         line_obj = self.pool.get('sale.order.line')
         order_obj = self.pool.get('sale.order')
-            
-        for wiz in self.browse(cr, uid, ids, context=context):            
+
+        for wiz in self.browse(cr, uid, ids, context=context):
             for line in wiz.order_id.order_line:
-                new_price = currency_obj.compute(cr, uid, wiz.old_pricelist_id.currency_id.id, wiz.new_pricelist_id.currency_id.id, line.price_unit, round=False, context=context)
-                line_obj.write(cr, uid, line.id, {'price_unit': new_price}, context=c)
-                
-            order_data = {'pricelist_id': wiz.new_pricelist_id.id,}
-            
-            order_obj.write(cr, uid, wiz.order_id.id, order_data, context=context)
-            
+                new_price = currency_obj.compute(
+                    cr,
+                    uid,
+                    wiz.old_pricelist_id.currency_id.id,
+                    wiz.new_pricelist_id.currency_id.id,
+                    line.price_unit,
+                    round=False,
+                    context=context
+                )
+                line_obj.write(cr, uid, line.id, {
+                    'price_unit': new_price,
+                }, context=c)
+
+            order_obj.write(cr, uid, wiz.order_id.id, {
+                'pricelist_id': wiz.new_pricelist_id.id,
+            }, context=context)
+
+            msg = _('The currency of the FO \'%s\' has been changed. '\
+                    '(old currency: %s - new currency: %s)') % (
+                        wiz.order_id.name,
+                        wiz.old_pricelist_id.currency_id.name,
+                        wiz.new_pricelist_id.currency_id.name,
+                    )
+            self.infolog(cr, uid, msg)
+
         return {'type': 'ir.actions.act_window_close'}
-    
+
 sale_order_change_currency()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
