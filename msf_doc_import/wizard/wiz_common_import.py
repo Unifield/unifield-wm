4aefd529b75c795699520efb2dabade7b4ee6cbf
Quentin THEURET <qt@tempo-consulting.fr>
2015-02-20 11:30:10 +0100

    [FIX] Fix bad check on context

--- msf_doc_import/wizard/wiz_common_import.py
+++ msf_doc_import/wizard/wiz_common_import.py
@@ -286,7 +286,7 @@ class product_product(osv.osv):
         return res
 
     def _check_concurrency(self, cr, ids, context=None):
-        if context.get('add_multiple_lines'):
+        if context and context.get('add_multiple_lines'):
             return
 
         return super(product_product, self)._check_concurrency(cr, ids, context=context)
