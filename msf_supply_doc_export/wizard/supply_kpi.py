76ce9585aed5b1176a7997d82bda6b27afc47e11
Stephane Codazzi stephane.codazzi@tempo-consulting.fr <>
2015-04-21 14:36:50 +0200

    Fix database

--- msf_supply_doc_export/wizard/supply_kpi.py
+++ msf_supply_doc_export/wizard/supply_kpi.py
@@ -23,7 +23,7 @@ from osv import osv
 from osv import fields
 import threading
 import pooler
-from logging import Logger
+import logging
 from datetime import datetime
 from datetime import timedelta
 
@@ -191,7 +191,7 @@ class supply_kpi(osv.osv):
 
     def refresh_thread(self, cr, uid, kpi_id, context=None):
         print "Start refreshing KPI at " + str(datetime.now())
-        Logger.error("Start refreshing KPI at " + str(datetime.now()))
+        logging.info("KPI: Start refreshing KPI at " + str(datetime.now()))
         cr = pooler.get_db(cr.dbname).cursor()
         kpi_obj = self.pool.get('kpi.refresh')
         kpi_obj.truncate_tables(cr, uid)
@@ -199,7 +199,7 @@ class supply_kpi(osv.osv):
         values = {'running': False}
         super(supply_kpi, self).write(cr, uid, kpi_id, values, context=context)
         print "Stop refreshing KPI at " + str(datetime.now())
-        Logger.error("Stop refreshing KPI at " + str(datetime.now()))
+        logging.info("KPI: Stop refreshing KPI at " + str(datetime.now()))
         cr.commit()
         cr.close()
 
