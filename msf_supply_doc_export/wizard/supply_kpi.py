0fa557718bced2a5778b910696e367766d72aef8
Stephane Codazzi stephane.codazzi@tempo-consulting.fr <>
2015-04-22 09:58:50 +0200

    Fix summary fields

--- msf_supply_doc_export/wizard/supply_kpi.py
+++ msf_supply_doc_export/wizard/supply_kpi.py
@@ -160,12 +160,13 @@ class supply_kpi(osv.osv):
 
         if kpi_id:
             kss_obj = self.pool.get('supply.kpi.summary')
-            kss_id = kss_obj.search(cr, uid, [(uid, '=', uid)], context=context)[0]
-            kss = kss_obj.browse(cr, uid, kss_id, context)
-            kpi_id['dim_3a'] = kss.dim_3a
-            kpi_id['dim_6a'] = kss.dim_6a
-            kpi_id['dim_6a_currency'] = kss.dim_6a_currency
-            kpi_id['dim_8b'] = kss.dim_8b
+            kss_ids = kss_obj.search(cr, uid, [(uid, '=', uid)], context=context)
+            if kss_ids[0]:
+                kss = kss_obj.browse(cr, uid, kss_ids[0], context)
+                kpi_id['dim_3a'] = kss.dim_3a
+                kpi_id['dim_6a'] = kss.dim_6a
+                kpi_id['dim_6a_currency'] = kss.dim_6a_currency
+                kpi_id['dim_8b'] = kss.dim_8b
         return kpi_id
 
     def check_kpi_running(self, cr, uid, context=None):
