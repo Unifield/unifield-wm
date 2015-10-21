8e265d36bb0a1f7d905a336de609f8ef1ddc37c7
Quentin THEURET <qt@tempo-consulting.fr>
2015-10-21 15:14:37 +0200

    SP-209 [IMP] Move unifield test data into a specific module to avoid installation of sync_server module on non sync. server databases

---
+++ unifield_tests_data/__openerp__.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2011 TeMPO Consulting, MSF. All Rights Reserved
+#    Developer: Olivier DOSSMANN
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    "name" : "Data for Unifield Unit Tests",
+    "version" : "0.1",
+    "description" : "This module adds data for unit tests for Unifield modules",
+    "author" : "TeMPO Consulting, MSF",
+    "category" : "Tests",
+    "depends" : [
+        'base',
+    ],
+    "init_xml" : [
+        'data/010_accounts.yml',
+        'data/020_partners.yml',
+        'data/030_journals.yml',
+        'data/040_analytic.yml',
+        'data/050_product_nomenclatures.yml',
+        'data/060_product_categories.yml',
+        'data/070_products.yml',
+        'data/080_locations.yml',
+    ],
+    "update_xml" : [],
+    "demo_xml" : [],
+    "test": [],
+    "installable": True,
+    "active": False
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
