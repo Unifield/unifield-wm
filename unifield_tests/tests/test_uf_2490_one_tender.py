728697089ea031aea1070e680f9fbf9e9a5de6c1
Quentin THEURET <qt@tempo-consulting.fr>
2015-10-27 09:54:58 +0100

    SP-209 [IMP] Unit tests: Add copyright header on all test files

--- unifield_tests/tests/test_uf_2490_one_tender.py
+++ unifield_tests/tests/test_uf_2490_one_tender.py
@@ -1,7 +1,25 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2014 TeMPO Consulting, MSF. All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
-__author__ = 'qt'
 
 from test_uf_2490_one_po import ResourcingTest
 
