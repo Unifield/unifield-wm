ed35ecb6862b897074e579b52f65c5e6fec3e91a
VG <vg@tempo-consulting.fr>
2015-11-10 14:29:50 +0100

    [IMP] key/value storage tree view

--- unifield_tests/__openerp__.py
+++ unifield_tests/__openerp__.py
@@ -39,6 +39,7 @@
         'automatic_tests/views/automatic_test_template_view.xml',
         'automatic_tests/views/automatic_test_campaign_view.xml',
         'automatic_tests/views/automatic_test_view.xml',
+        'automatic_tests/views/automatic_test_key_val_view.xml',
         'automatic_tests/wizard/views/automatic_test_add_file_view.xml',
     ],
     "demo_xml" : [],
