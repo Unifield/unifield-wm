1551b6117fa8823a7d8ef6d9dc0a49028ce5fab1
jf <jfb@tempo-consulting.fr>
2016-03-18 11:12:21 +0100

    US-1025 [FIX] Recoved update on sync server

--- sync_server/update.py
+++ sync_server/update.py
@@ -431,7 +431,7 @@ class update(osv.osv):
             filters.append("direction = 'down' AND source IN (" + (','.join(map(str,ancestor))) + ")")
         filters.append("direction = 'bidirectional'")
         if recover:
-            filters.append("source = " + entity.id)
+            filters.append("source = %s" % (entity.id,))
         filters.append("direction = 'bi-private' AND (is_deleted = 't' OR owner IN (" + (','.join(map(str, children + [entity.id]))) + "))")
         base_query += ' AND ((' + ') OR ('.join(filters) + '))'
 
