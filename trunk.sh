b481f68bae9cf6e9b16e26df5a3aa58eb7f5f451
Jeff Allen <jeff.allen@geneva.msf.org>
2017-04-13 17:31:11 +0200

    Checkpoint of Python 2.7 AIO build system.

---
+++ trunk.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SERVER_BRANCH=lp:unifield-server/trunk
+CLIWEB_BRANCH=lp:unifield-web/trunk
+[ -n "$1" ] && SERVER_BRANCH=$1
+[ -n "$2" ] && CLIWEB_BRANCH=$2
+
+br=`basename $SERVER_BRANCH`
+
+./package_uf5.py \
+        --version uf5.0-$br \
+        --server-branch=$SERVER_BRANCH \
+        --client-web-branch=$CLIWEB_BRANCH \
+        $@
+
