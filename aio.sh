5593505308a2bc4da8bb2077c5b9c7bdea7a8fe7
Jeff Allen <jeff.allen@geneva.msf.org>
2017-07-11 09:40:02 +0200

    Make the driver script more usable.

--- aio.sh
+++ aio.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+if [ "$1" = "-h" ]; then
+	echo "usage: $0 [server-branch] [web-branch]"
+	exit 0
+fi
+
+set -e
 SERVER_BRANCH=lp:unifield-server/trunk
 CLIWEB_BRANCH=lp:unifield-web/trunk
 [ -n "$1" ] && SERVER_BRANCH=$1
 [ -n "$2" ] && CLIWEB_BRANCH=$2
+
+echo "Server branch: $SERVER_BRANCH"
+echo "Web branch: $CLIWEB_BRANCH"
 
 br=`basename $SERVER_BRANCH`
 
