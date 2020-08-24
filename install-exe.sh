a82d0fe7f988926e2cb5f54d65e619134b606aec
jf <jfb@tempo-consulting.fr>
2020-08-24 11:50:58 +0200

    Script to install / uninstall OpenERP on Win

---
+++ install-exe.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ssh_user=JFTEMP
+ssh_host=win10
+ssh_port=22
+# Driver for mk-patch.py, which is run from INSIDE of the builder VM.
+
+if [ -z "$1" ]; then
+    echo "usage: $0 exe"
+    exit 1
+fi
+
+exe=$1
+
+
+ssh_args="-p $ssh_port $ssh_user@$ssh_host"
+# Put the script and the two files on the VM
+echo "Copying exe files over to the VM."
+scp -P $ssh_port $exe $ssh_user@$ssh_host:
+
+exe=`basename $exe`
+
+ssh $ssh_args chmod +x $exe
+ssh $ssh_args TZ="Europe/Paris" ./$exe /S
+
