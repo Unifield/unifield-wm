a3d4695adcfeb87985ccbe55b3e6c2cad28cae9e
jftempo <jfb@tempo-consulting.fr>
2026-02-25 11:26:20 +0100

    FX rates Feb 2026, remove 2023 rates

--- data/generate_rate.py
+++ data/generate_rate.py
@@ -4,7 +4,7 @@ from dateutil.relativedelta import relativedelta
 
 date_now = datetime.now() + relativedelta(day=1)
 #for base in ['eur', 'chf']:
-date_from = datetime.strptime('2023-01-01', '%Y-%m-%d')
+date_from = datetime.strptime('2024-01-01', '%Y-%m-%d')
 feur = open('eur.txt', 'w')
 fchf = open('chf.txt', 'w')
 
